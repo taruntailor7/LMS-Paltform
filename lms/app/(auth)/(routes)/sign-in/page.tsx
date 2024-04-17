c7126d392dbcb2a4b94212e32ff2b9cc4200faaf
Tarun Tailor <tarun@crimecheck.in>
2024-04-17 23:55:09 +0530

    learned creating routes, api, (groups)

---
+++ lms/app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+    return ( 
+        <div>
+            This is an unprotected Page.
+        </div> 
+    );
+}
+ 
+export default SignInPage;
---
+++ lms/app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <p>This is a protected Page.</p>
+    </div>
+  )
+}
