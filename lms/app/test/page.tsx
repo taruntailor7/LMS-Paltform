b6ff5715236d912a5cb6d370685a2d24496a0218
Tarun Tailor <tarun@crimecheck.in>
2024-04-18 20:51:14 +0530

    if we add route in .env that means those rouets are unprotected clerk handle this by it's own and if we want to do the same then we can add publicRoutes in middleware.ts by that we can unprotect routes else all routes will be protected.

---
+++ lms/app/test/page.tsx
@@ -0,0 +1,6 @@
+const test = () => {
+    // added publicRoutes : ['/test'] in middleware.ts for unprotecting this route
+    return ( <p>this is unprotected route</p> );
+}
+ 
+export default test;
--- lms/middleware.ts
+++ lms/middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+    // 1st way to unprotect route is to add in .env variables clerk will automatically unprotect those routes.
+    publicRoutes : ['/test'] // This is 2nd way -> here we can add public routes which can be accessed by everyone even logged out user also can see.
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
