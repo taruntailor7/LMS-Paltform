f74b2f09335b5366fda8e1c23944837fe8f3ff96
Tarun Tailor <taruntailor7@gmail.com>
2024-04-20 17:20:52 +0530

    side bar and navabr added for web and mobile for both

--- lms/app/(dashboard)/layout.tsx
+++ lms/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "./_components/navbar";
 import Sidebar from "./_components/sidebar";
 
 
@@ -5,7 +6,7 @@ const DashboardLayout = ({children} : {children: React.ReactNode}) => {
     return ( 
         <div className="h-full">
             <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
-                
+                <Navbar />  
             </div>
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
