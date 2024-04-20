0bad10c72bd83f6aea773c6db613a5a785f84dce
Tarun Tailor <taruntailor7@gmail.com>
2024-04-20 20:06:15 +0530

    teacher mode added and in that rouets created for courses and analytics and also create route for creating course and for creating course form also creating from shadcn form now will make api for for creating course by sending api request on /api/course then we will be able to create cousre so for that now will do prisma and db setup

--- lms/components/ui/button.tsx
+++ lms/components/ui/button.tsx
@@ -9,14 +9,14 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-black text-white hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-slate-300/20",
         link: "text-primary underline-offset-4 hover:underline",
         sky: "bg-sky-700 text-white"
       },
