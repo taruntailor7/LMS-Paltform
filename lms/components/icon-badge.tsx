3a824a3158479434136f777daea76e34289c9234
Tarun Tailor <taruntailor7@gmail.com>
2024-04-21 00:02:30 +0530

    custom icon-badge created

--- lms/components/icon-badge.tsx
+++ lms/components/icon-badge.tsx
@@ -1,2 +1,28 @@
 import { LucideIcon } from 'lucide-react';
+import { cva, type, VariantProps } from "class-variance-authority";
 
+import { cn } from "@/lib/utils";
+
+const backgroundVariants = cva(
+    "rounded-full flex items-center justify-center",
+    {
+        variants: {
+            variant: {
+                default: "bg-sky-100",
+                success: "bg-emerald-100"
+            },
+            iconVariant: {
+                default: "text-sky-700",
+                success: "text-emerald-700"
+            },
+            size: {
+                default: "p-2",
+                sm: "p-1"
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        }
+    }
+);
