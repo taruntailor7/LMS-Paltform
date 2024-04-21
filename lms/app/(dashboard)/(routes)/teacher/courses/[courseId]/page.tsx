020b67058dab3feecbba1ca531ea33869eefb500
Tarun Tailor <taruntailor7@gmail.com>
2024-04-21 23:16:43 +0530

    Created edit course title form by patch request

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,8 @@ import { LayoutDashboard } from "lucide-react";
 
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
+ 
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: {courseId: string} }) => {
     const { userId } = auth();
@@ -56,6 +58,10 @@ const CourseIdPage = async ({ params }: { params: {courseId: string} }) => {
                             Customize your course
                         </h2>
                     </div>
+                    <TitleForm 
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>  
