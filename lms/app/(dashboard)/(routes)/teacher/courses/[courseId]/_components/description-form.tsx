5d876882062b294986c360039a28adce446236d6
Tarun Tailor <taruntailor7@gmail.com>
2024-04-22 23:36:02 +0530

    Uplaod Image form completed ui and also functionality from uplaodthing and now we are able to uplaod image for that i need to make /api/uploadthing route public so that i can access that and upload image

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -18,7 +18,6 @@ import {
     FormItem,
     FormMessage
 } from "@/components/ui/form";
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 
--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -2,26 +2,14 @@
 
 import * as z from 'zod';
 import axios from 'axios';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
 import { ImageIcon, Pencil, PlusCircle } from 'lucide-react';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
-import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import { Course } from '@prisma/client';
 import Image from 'next/image';
 
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage
-} from "@/components/ui/form";
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { Textarea } from '@/components/ui/textarea';
 import { FileUpload } from '@/components/file-upload';
 
 interface ImageFormProps {
@@ -40,15 +28,6 @@ export const ImageForm = ({initialData, courseId}: ImageFormProps) => {
 
     const toggleEdit = () => setIsEditing((current)=> !current);
     const router = useRouter();
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            imageUrl: initialData?.imageUrl || "",
-        }
-    });  
-
-    const { isSubmitting, isValid } = form.formState;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         // console.log("Values: ", values); 
