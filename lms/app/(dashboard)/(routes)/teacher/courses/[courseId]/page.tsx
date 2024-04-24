6d418e1c643118dfdf11bf0e6de737901c8faa02
Tarun Tailor <taruntailor7@gmail.com>
2024-04-25 00:09:22 +0530

    started working on category form and forthat installed combobox from shadcn and code also changed but not working so will see this tomorrow

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({ params }: { params: {courseId: string} }) => {
     const { userId } = auth();
@@ -21,6 +22,13 @@ const CourseIdPage = async ({ params }: { params: {courseId: string} }) => {
             id: params.courseId
         }
     });
+
+    const categories = await db.category.findMany({
+        orderBy : {
+            name: "asc"
+        }
+    });
+    // console.log("Categories: ", categories);
 
     if(!course) {
         return redirect('/');
@@ -71,6 +79,14 @@ const CourseIdPage = async ({ params }: { params: {courseId: string} }) => {
                     <ImageForm
                         initialData={course}
                         courseId={course.id}
+                    />
+                    <CategoryForm
+                        initialData={course}
+                        courseId={course.id}
+                        options={categories.map((category) => ({
+                            label: category.name,
+                            value: category.id,
+                        }))}
                     />
                 </div>
             </div>
