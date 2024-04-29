6d3c3f32756076a1d9f5fef8a6ea2b2a8da6ddc3
Tarun Tailor <taruntailor7@gmail.com>
2024-04-29 14:31:22 +0530

    started creating drag drop for chapterd and mapped also chapters

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -10,6 +10,7 @@ import {
 } from '@hello-pangea/dnd'
 
 import { cn } from '@/lib/utils'
+import { Grip } from "lucide-react";
 
 interface ChapterListProps {
     items: Chapter[];
@@ -38,8 +39,40 @@ export const ChaptersList = ({
     }
 
     return (
-        <div>
-            Chapters List
-        </div>
+        <DragDropContext onDragEnd={() => {}}>
+            <Droppable droppableId="chapters">
+                {(provided) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                        {chapters.map((chapter, index) => (
+                            <Draggable 
+                                key={chapter.id}
+                                draggableId={chapter.id}
+                                index={index}
+                            >
+                                {(provided) => (
+                                    <div 
+                                        className={cn(
+                                            "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm", 
+                                            chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
+                                        )}
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                    >
+                                        <div className={cn(
+                                            "px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                                            chapter.isPublished && "border-r-sky-200 hover:bg-sky-200"
+                                        )}
+                                        {...provided.dragHandleProps}
+                                        >
+                                            <Grip />
+                                        </div>
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     )
 }
