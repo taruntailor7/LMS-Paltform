f80391b375c45bbcd5691b4a22c965479743e863
Tarun Tailor <taruntailor7@gmail.com>
2024-04-30 00:09:06 +0530

    loader also added while drag drop for showing that it is wokring on dropping and also edit button enabled by writing onEdit function and now will make edit page UI

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapter-form.tsx
@@ -4,7 +4,7 @@ import * as z from 'zod';
 import axios from 'axios';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { PlusCircle } from 'lucide-react';
+import { Loader2, PlusCircle } from 'lucide-react';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { cn } from '@/lib/utils';
@@ -77,8 +77,17 @@ export const ChaptersForm = ({initialData, courseId}: ChaptersFormProps) => {
         }
     }
 
+    const onEdit = (id: string) => {
+        router.push(`/teacher/course/${courseId}/chapters/${id}`);
+    }
+
     return (  
-        <div className='mt-6 border bg-slate-100 rounded-md p-4'>
+        <div className='relative mt-6 border bg-slate-100 rounded-md p-4'>
+            {isUpdating && (
+                <div className='absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center'>
+                    <Loader2 className='animate-spin h-6 w-6 text-sky-700' />
+                </div>
+            )}
             <div className='font-medium flex items-center justify-between'>
                 Course chapters
                 <Button onClick={toggleCreating} variant="ghost">
@@ -130,7 +139,7 @@ export const ChaptersForm = ({initialData, courseId}: ChaptersFormProps) => {
                 )}>
                     {!initialData.chapters.length && "No Chapters"}
                     <ChaptersList 
-                        onEdit={() => {}}
+                        onEdit={onEdit}
                         onReorder={onReorder}
                         items={initialData.chapters || []}
                     />
