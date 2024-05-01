0c7300528758786895cc553f04c4619afd9d420d
Tarun Tailor <taruntailor7@gmail.com>
2024-05-01 18:21:39 +0530

    for publish also function created

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -26,6 +26,29 @@ export const ChapterActions = ({
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
 
+    const onClick = async () => {
+        try {
+            setIsLoading(true);
+
+            if (isPublished) {
+                await axios.patch(`api/courses/${courseId}/chapters/${chapterId}/unpublish`);
+                toast.success('Chapter unpublished');
+            } else {
+                await axios.patch(`api/courses/${courseId}/chapters/${chapterId}/publish`);
+                toast.success('Chapter published');
+            }
+
+            // toast.success('Chapter deleted');
+            // // router.refresh();
+            // router.push(`/teacher/courses/${courseId}`);
+            // router.refresh();
+        } catch (error) {
+            toast.error("Something went wrong")
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     const onDelete = async () => {
         try {
             setIsLoading(true);
@@ -46,7 +69,7 @@ export const ChapterActions = ({
     return (
         <div className="flex items-center gap-x-2">
             <Button
-                onClick={() => {}}
+                onClick={onClick}
                 disabled={disabled}
                 variant="outline"
                 size="sm"
