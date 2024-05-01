082b0ace3df0fbbe773fa8ac4af7d9aa7b1aba02
Tarun Tailor <taruntailor7@gmail.com>
2024-05-01 22:43:14 +0530

    data table created from shadcn for showing all courses in table in course route and also pagination and sorting added from documentation of shadcn

--- lms/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,35 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { db } from "@/lib/db";
+
+
+const CoursesPage = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId
+        },
+        orderBy: {
+            createdAt: "desc",
+        }
+    })
+
     return ( 
         <div className="p-6"> 
-            <Link href='/teacher/create'>
+            <DataTable columns={columns} data={courses} />
+            {/* <Link href='/teacher/create'>
                 <Button>
                     New Course
                 </Button>
-            </Link>
+            </Link> */}
         </div>
     );
 }
