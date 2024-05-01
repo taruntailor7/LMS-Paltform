8c5d53ecd72c124a4c7291f583d3c25e23003013
Tarun Tailor <taruntailor7@gmail.com>
2024-05-01 23:27:11 +0530

    price and published status modfied or designed for good looking

--- lms/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -2,11 +2,13 @@
 
 import Link from "next/link"
 import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 
 import { Button } from "@/components/ui/button" 
+import { Badge } from "@/components/ui/badge"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -43,6 +45,15 @@ export const columns: ColumnDef<Course>[] = [
             </Button>
         );
     },
+    cell: ({ row }) => {
+        const price = parseFloat(row.getValue("price") || "0");
+        const formatted = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(price);
+
+        return <div>{formatted}</div>
+    }
   },
   {
     accessorKey: "isPublished",
@@ -55,8 +66,20 @@ export const columns: ColumnDef<Course>[] = [
                 Published
                 <ArrowUpDown className="ml-2 h-4 w-4" />
             </Button>
-        );
+        )
     },
+    cell: ({ row }) => {
+        const isPublished = row.getValue("isPublished") || false;
+
+        return (
+            <Badge className={cn(
+                "bg-slate-500",
+                isPublished && "bg-sky-700"
+            )}>
+                {isPublished ? "Published" : "Draft"}
+            </Badge>
+        )
+    }
   },
   {
     id: "actions",
