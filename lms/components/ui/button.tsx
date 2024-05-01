c3224f6486635a01d048525bf2bf7fa3b9fa0b10
Tarun Tailor <taruntailor7@gmail.com>
2024-05-01 17:25:27 +0530

    chapter actions created where publish and delete chapter features are there and from that delete ferature is completed from frontend side now going to make api for deleting chapter

--- lms/components/ui/button.tsx
+++ lms/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-slate-300/20",
@@ -23,6 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
+        md: "h-10 rounded-md px-4",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
