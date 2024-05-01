bc63da9a0d6776967dc30f232a69d3f6ca970090
Tarun Tailor <taruntailor7@gmail.com>
2024-05-01 11:43:56 +0530

    chapter title description form created and working aslo fine

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 import { db } from "@/lib/db";
 import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
+
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
     params
@@ -78,6 +80,11 @@ const ChapterIdPage = async ({
                         </div>
                         {/* ChapterTitleForm */}
                         <ChapterTitleForm
+                            initialData={chapter}
+                            courseId={params.courseId}
+                            chapterId={params.chapterId}
+                        />
+                        <ChapterDescriptionForm
                             initialData={chapter}
                             courseId={params.courseId}
                             chapterId={params.chapterId}
