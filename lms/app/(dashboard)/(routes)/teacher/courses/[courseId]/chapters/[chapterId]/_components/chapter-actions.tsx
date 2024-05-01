fa8ce40a4f5fc5481df23d8af541990f3466c81f
Tarun Tailor <taruntailor7@gmail.com>
2024-05-01 17:50:26 +0530

    delete chapter api is also created

--- lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -33,8 +33,9 @@ export const ChapterActions = ({
             await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
 
             toast.success('Chapter deleted');
+            // router.refresh();
+            router.push(`/teacher/courses/${courseId}`);
             router.refresh();
-            router.push(`/teacher/course/${courseId}`);
         } catch (error) {
             toast.error("Something went wrong")
         } finally {
