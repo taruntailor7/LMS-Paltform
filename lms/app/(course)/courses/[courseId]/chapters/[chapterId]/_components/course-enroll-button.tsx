db0a8c753b9a9559c4c0393c1d05bc80cee584cf
Tarun Tailor <taruntailor7@gmail.com>
2024-05-04 23:02:55 +0530

    webhook api is also created for making payment

--- lms/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ lms/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,5 +1,9 @@
+import axios from "axios"
 import { Button } from "@/components/ui/button"
+import toast from "react-hot-toast"
+
 import { formatePrice } from "@/lib/format"
+import { useState } from "react"
 
 interface CourseEnrollButtonProps {
     price: number,
@@ -10,6 +14,22 @@ export const CourseEnrollButton = ({
     price,
     courseId,
 }: CourseEnrollButtonProps) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onClick = async () => {
+        try {
+            setIsLoading(true);
+
+            const response = await axios.post(`/api/courses/${courseId}/checkout`)
+
+            window.location.assign(response.data.url);
+        } catch (error) {
+            toast.error("Something went wrong");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <Button
             size="sm"
