915b9a77a5cbf9cccca5103c6fcd93995f49a442
Tarun Tailor <taruntailor7@gmail.com>
2024-05-04 14:43:51 +0530

    mobile sidebar and navbar both designed and created

--- lms/components/navbar-routes.tsx
+++ lms/components/navbar-routes.tsx
@@ -11,16 +11,18 @@ export const NavbarRoutes = () => {
     const pathname = usePathname();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
-    const isPlayerPage = pathname?.includes("/chapter");
+    const isCoursePage = pathname?.includes("/courses");
     const isSearchPage = pathname === "/search";
 
     return (
         <>
-            <div className="hidden md:block">
-                <SearchInput />
-            </div>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput />
+                </div>
+            )}
             <div className="flex gap-x-2 ml-auto">
-                {isTeacherPage || isPlayerPage ? (
+                {isTeacherPage || isCoursePage ? (
                     <Link href="/">
                         <Button size="sm" variant="ghost">
                             <LogOut className="h-4 w-4 mr-2"/>
