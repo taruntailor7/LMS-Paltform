0464475084a3ab3a3e3d77be6ee6bd29ebee417c
Tarun Tailor <taruntailor7@gmail.com>
2024-05-05 12:33:15 +0530

    onchapter end redirect to next chapter featrue also add

--- lms/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ lms/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -30,8 +30,31 @@ export const VideoPlayer = ({
     title,
 }: VideoPlayerProps) => {
     const [isReady, setIsReady] = useState(false);
+    const router = useRouter();
+    const confetti = useConfettiStore();
 
+    const onEnd = async () => {
+        try {
+            if (completeOnEnd) {
+                await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
+                    isComplete: true,
+                });
 
+                if (!chapterId) {
+                    confetti.onOpen();
+                }
+
+                toast.success("Progress upadted");
+                router.refresh();
+
+                if (chapterId) {
+                    router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+                }
+            }
+        } catch (error) {
+            toast.error("Something went wrong");
+        }
+    }
 
     return (
         <div className="relative aspect-video">
