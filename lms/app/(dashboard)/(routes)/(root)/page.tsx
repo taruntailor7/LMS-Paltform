d3fcb2d1c8dc2f57c2b5e26b13b0ff50966b9dd2
Tarun Tailor <taruntailor7@gmail.com>
2024-05-05 13:18:25 +0530

    student dashboard page completed going to create teacher analytics page

--- lms/app/(dashboard)/(routes)/(root)/page.tsx
+++ lms/app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,10 +1,42 @@
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation';
+import { CheckCircle, Clock } from 'lucide-react';
 
-export default function Dashboard() {
+import { getDashboardCourses } from '@/actions/get-dashboard-courses';
+import { CourseList } from "@/components/courses-list";
+
+import { InfoCard } from './_components/info-card';
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { 
+    completedCourses,
+    coursesInProgress
+  } = await getDashboardCourses(userId);
+
   return (
-    <div>
-
+    <div className='p-6 space-y-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={completedCourses.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={coursesInProgress.length}
+          variant='success'
+        />
+      </div>
+      <CourseList 
+        items={[...coursesInProgress, ...completedCourses]} 
+      />
     </div>
   )
 }
